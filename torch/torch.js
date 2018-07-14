f3790bfbe105107044b8f9bdbcd258c6b005b1ca
Susan Chen <S529chen@uwaterloo.ca>
2018-07-14 15:49:44 -0400

    torch

--- torch/torch.js
+++ torch/torch.js
@@ -1,4 +1,6 @@
-document.body.style.cursor = "url('torch.png')";
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 var svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
 var svgNS = svg.namespaceURI;
@@ -10,7 +12,7 @@ svg.style.height=height*2;
 svg.style.width = width*2;
 svg.style.left="-0%";
 svg.style.top="-0%";
-svg.style.cursor = "url('torch.png')";
+svg.style.opacity = "0.95";
 
 newpath = document.createElementNS(svgNS,"path");
 newpath.setAttribute("id","hover-circle");
@@ -19,9 +21,25 @@ newpath.setAttributeNS(null, "fill-rule", "evenodd");
 svg.appendChild(newpath);
 document.body.prepend(svg);
 
-onmousemove = function(e) {
+curx = 0;
+cury = 0;
+
+window.setInterval(function(){
     path= document.getElementById("hover-circle");
-    x = e.clientX;
-    y = e.clientY;
+    x = curx + getRandomInt(-30,30);
+    y = cury + getRandomInt(-30,30);
     path.setAttribute("d", "M0,0 " + width + ",0 " + width + "," + height + " 0," + height + " M"+(x-50)+","+y+"a50,50 0 1,0 100,0a50,50 0 1,0 -100,0 z");
+  }, 20);
+
+onmousemove = function(e) {
+    curx = e.clientX;
+    cury = e.clientY;
 }
+
+function addStyleString(str) {
+    var node = document.createElement('style');
+    node.innerHTML = str;
+    document.body.appendChild(node);
+}
+
+addStyleString('body { cursor: url("./torch.png") 2 2, pointer; }');
